09cd4fba90998495eaf7665275794c5377508361
Andrés Guachamín <andres_jgb05@hotmail.com>
2026-09-16 22:33:34 -0500

    App 2

--- Clase 9/App.java
+++ Clase 9/App.java
@@ -1,24 +1,32 @@
 /*
 Titulo: Ejercicio 2 - Constructor y metodos
-Autor: Andrés Guachamín
-Enunciado: 
+Autor: Jhoantan Torres
+Enunciado:
     Crear una clase llamada Producto con los atributos nombre, precio y stock.
-    La clase debe tener un constructor, un metodo para mostrar la información
-    y un método para calcular el valor total de inventario.
+    La clase debe tener un constructor, un metodo para mostrar informacion 
+    y un metodo para calcular el valor total de inventario
 */
 
 public class App {
     public static void main(String[] args) throws Exception {
         // Creamos el primer objeto usando el constructor
-        Producto producto1 = new Producto(nombre: "Mouse", precio: 15.5, stock: 10);
+        Producto producto1 = new Producto("Mouse", 15.50, 10);
 
         // Creamos el segundo objeto usando el constructor
-        Producto producto2 = new Producto(nombre: "Teclado", precio: 25.0, stock: 5);
+        Producto producto2 = new Producto("Teclado", 25.00, 5);
 
-        System.out.println("Información del producto 1:");
+        System.out.println("=== PRODUCTO 1 ===");
+        // Llamamos al metodo mostrarInfo del primer objeto
+        producto1.mostrarInfo();
+        
+        System.out.println("Valor total en inventario: " + producto1.calcularValorInventario());
 
-        // Calculamos y mostramos el valor total de inventario
-        double valorTotalInventario = producto1.calcularValorTotalInventario();
-        System.out.println("Valor total de inventario: " + valorTotalInventario);
+        System.out.println();
+
+        System.out.println("==== PRODUCTO 2 ===");
+        //Llamamos a la metodo
+        producto2.mostrarInfo();
+        
+        System.out.println("Valor total en inventario: " + producto2.calcularValorInventario());
     }
 }
