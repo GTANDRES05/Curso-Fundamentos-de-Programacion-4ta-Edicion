d316c4c6c9dee749a582f3c89c6991e6a2e7f4e1
Andrés Guachamín <andres_jgb05@hotmail.com>
2026-09-21 21:19:56 -0500

    Ejercicio 1: clase Mascota

---
+++ tareaPOO/ejercicio1/src/Mascota.java
@@ -0,0 +1,24 @@
+/*
+Clase Mascota
+Representa una mascota sencilla con nombre, especie y edad.
+Aqui se practica: clase, atributos, constructor y metodo simple.
+*/
+public class Mascota {
+
+    String nombre;
+    String especie;
+    int edad;
+
+    public Mascota(String nombre, String especie, int edad) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad: " + edad + " años");
+        System.out.println("------------------------");
+    }
+}
