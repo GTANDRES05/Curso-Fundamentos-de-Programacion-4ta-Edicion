6d2df7a0186aaa725e474635882d28329a8b0f01
Andrés Guachamín <andres_jgb05@hotmail.com>
2026-09-21 21:21:40 -0500

    Ejercicio 1: clase App con dos objetos

--- tareaPOO/ejercicio1/src/App.java
+++ tareaPOO/ejercicio1/src/App.java
@@ -1,5 +1,15 @@
+/*
+Clase principal App - Ejercicio 1
+Crea dos objetos de tipo Mascota y muestra su informacion en consola.
+*/
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Mascota mascota1 = new Mascota("Sky", "Perro", 3);
+        Mascota mascota2 = new Mascota("Pascal", "Gato", 2);
+
+        System.out.println("=== Informacion de las mascotas ===");
+        mascota1.mostrarInfo();
+        mascota2.mostrarInfo();
     }
 }
